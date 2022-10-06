e937b43956c665dd3e2d18a2818ffbec8de6b152
Dinesh Thakare <dineshthakare318@gmail.com>
2022-10-06 12:51:17 +0530

    js4

---
+++ for.js
@@ -0,0 +1,9 @@
+var total=0;
+for(var i=0;i<5;i++)
+{
+    var j=prompt("Please enter value");
+    total=total+parseInt(j);
+
+}
+document.write(total);
+var text="";
---
+++ while.js
@@ -0,0 +1,12 @@
+var total=0;
+var i=0;
+while(i<5)
+{
+    var j=prompt("enter value");
+    total=total+parseInt(j);
+
+    
+
+}
+document.write(total);
+var text="";
